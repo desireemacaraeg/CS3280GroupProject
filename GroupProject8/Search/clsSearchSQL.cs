6ee31c979d9ed74554b09182e6a51cb9ca0dfc30
Desiree Macaraeg <W01322502@cs.weber.edu>
2020-11-03 10:18:45 -0700

    Created folders, classes, and windows

---
+++ GroupProject8/Search/clsSearchSQL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject8.Search
+{
+    class clsSearchSQL
+    {
+    }
+}
